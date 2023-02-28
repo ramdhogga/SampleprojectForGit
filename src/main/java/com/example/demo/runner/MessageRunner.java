7ed35dec59b984596f9f66c9da36feb8c001bc0e
chinnammalu <chinnammalupeddinti@gmail.com>
2023-02-28 13:29:54 +0530

    wh-501-update log4j using slf4j

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -1,12 +1,16 @@
 package com.example.demo.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
 public class MessageRunner implements CommandLineRunner {
-
+    private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
+		log.info("Started");
 		System.out.println("welecome git account");
+		log.info("Ended");
 	}
 
 }
