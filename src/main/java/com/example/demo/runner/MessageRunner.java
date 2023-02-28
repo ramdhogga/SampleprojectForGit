7866cec36265332644b9813533bec5b033d142ee
chinnammalu <chinnammalupeddinti@gmail.com>
2023-02-28 13:08:58 +0530

    fifth commit

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -7,7 +7,6 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("welecome git account");
-		System.out.println("file is modified");
 	}
 
 }
